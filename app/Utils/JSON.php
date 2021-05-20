44c0a93a17cf7b78e3d64d95ae3cb11db150fa0a
Senpai 20.04 <senpai.codes@gmail.codes>
2021-05-20 13:39:36 +0200

    JSON

---
+++ app/Utils/JSON.php
@@ -0,0 +1,108 @@
+<?php
+namespace WP_SENPAI\Utils;
+class JSON {
+	var $options;
+	var $option_name;
+    var $is_site_option;
+    
+    //if ( !defined( 'WPINC' ) ) {die();}
+
+	public function __construct($option_name, $is_site_options = false){
+		$this->option_name = $option_name;
+		$this->is_site_option = $is_site_options;
+		if($this->is_site_option){
+			$this->options = get_site_option($this->option_name);
+		} else {
+			$this->options = get_option($this->option_name);
+		}
+        
+		// Check if options are JSON
+		if(!is_array($this->options)){
+			$temp_options = json_decode($this->options);
+			if(json_last_error() == JSON_ERROR_NONE && !empty($temp_options)){
+				$this->options = $temp_options;
+			}
+
+			if(empty($this->options)){
+				$this->options = array();
+			}
+		}
+	}
+ 
+    public function remove($key){
+        if(is_array($this->options)){
+            if(isset($this->options[$key])){
+                unset($this->options[$key]);
+                return true;
+            }
+        }elseif(is_object($this->options)){
+            if(isset($this->options->{$key})){
+                unset($this->options->{$key});
+                return true;
+            }
+        }
+        return false;
+    }
+
+
+    public function get($key){
+		if(is_array($this->options)){
+			if(isset($this->options[$key])){
+				return $this->options[$key];
+			}
+		} elseif(is_object($this->options)){
+			if(isset($this->options->{$key})){
+				return $this->options->{$key};
+			}
+		}
+
+		return false;
+    }
+    
+
+    public function get_all(){
+		if(is_array($this->options)){
+				return $this->options;
+		} elseif(is_object($this->options)){
+			$array = (array) $this->options;
+				return $array;
+		}
+		return false;
+	}
+ 
+	public function set($key, $value){
+		if(is_array($this->options)){
+			$this->options[$key] = $value;
+		} elseif(is_object($this->options)){
+			$this->options->{$key} = $value;
+		} else {
+			// Do nothing
+		}
+	}
+ 
+	function __isset($key){
+		if(is_array($this->options)){
+			return array_key_exists($key, $this->options);
+		} elseif(is_object($this->options)){
+			return property_exists($this->options, $key);
+		} else {
+			return false;
+		}
+	}
+ 
+	public function save(){
+		if($this->is_site_option){
+			update_site_option($this->option_name, json_encode($this->options));
+		} else {
+			update_option($this->option_name, json_encode($this->options));
+		}
+    }
+    
+	public function reset(){
+		if($this->is_site_option){
+			delete_option($this->option_name);
+		} else {
+			delete_option($this->option_name);
+		}
+    }
+}
---
+++ vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-main',
+    'version' => 'dev-main',
+    'aliases' => 
+    array (
+    ),
+    'reference' => 'f95de2fb40ea19a1442bae8e6b44d1b10d9ccd9d',
+    'name' => 'senpai/wp-senpai',
+  ),
+  'versions' => 
+  array (
+    'senpai/wp-senpai' => 
+    array (
+      'pretty_version' => 'dev-main',
+      'version' => 'dev-main',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'f95de2fb40ea19a1442bae8e6b44d1b10d9ccd9d',
+    ),
+  ),
+);
