e095e442ae3976ba08a6c19fd3becd2314ed61dc
Senpai 20.04 <senpai.codes@gmail.codes>
2021-06-05 15:50:03 +0200

    wordk on DB handler

--- app/Utils/DB.php
+++ app/Utils/DB.php
@@ -8,13 +8,60 @@ if ( !defined( 'WPINC' ) ) {die();}
 	 * @author amine safsafi
 	 */
 class DB {
+	/**
+	 * @ignore
+	 */
+    var $senpai_db;
+	/**
+	 * @ignore
+	 */
+    var $senpai_prefix;
+	/**
+	 * @ignore
+	 */
+    var $senpai_remote;
 
 	/**
+	 * $senpai_db = \WP_SENPAI\Utils\DB($prefix,$db_obj);
+	 * @param string $prefix
+	 * @param object $db
 	 * @author amine safsafi
 	 * @return void
 	 */
-    public function __construct(){
-
+    public function __construct($prefix,$db = NULL){
+		if($db == NULL){
+			global $wpdb;
+			$this->senpai_db = $wpdb;
+			$this->senpai_prefix = $wpdb->prefix . $prefix . '_';
+			$this->senpai_remote = false;
+		}else{
+			$remote_db = new wpdb($db->username,$db->password,$db->database_name,$db->host);
+			$this->senpai_db = $remote_db;
+			$this->senpai_remote = true;
+		}
     }
+
+	/**
+	 * $data = $senpai_db->get_all('senpai_table');
+	 * @param string $table_name
+	 * @return array
+	 */
+    public function get_all($table_name){
+		$table = $this->senpai_prefix . $table_name;
+		return $this->senpai_db->get_results( "SELECT * FROM $table_name" );
+	}
+
+	/**
+	 * $val = $senpai_db->get_value('senpai_table','senpai_col', id);
+	 * @param string $table_name
+	 * @param string $column_name
+	 * @param string $id
+	 * @return string
+	 */
+    public function get_value($table_name,$column_name,$id){
+		$table = $this->senpai_prefix . $table_name;
+		return $this->senpai_db->get_var( $wpdb->prepare("SELECT %s FROM %s WHERE id='%s'",array($column_name,$table,$id)));
+	}
+
 
 }
---
+++ app/Utils/DB_INIT.php
@@ -0,0 +1,55 @@
+<?php
+namespace WP_SENPAI\Utils;
+
+if ( !defined( 'WPINC' ) ) {die();}
+
+    /**
+	 * DB_INIT helper class it's used to speedup interact with WordPress DataBase while activate/deactivate theme or plugin.
+	 * @category Class
+	 * @author amine safsafi
+	 */
+
+require_once( ABSPATH . 'wp-admin/includes/upgrade.php' );
+
+class DB_INIT {
+	/**
+	 * @ignore
+	 */
+    var $senpai_db;
+	/**
+	 * @ignore
+	 */
+    var $senpai_prefix;
+    /**
+	 * @ignore
+	 */
+    var $senpai_charset;
+
+
+	/**
+	 * $senpai_db = \WP_SENPAI\Utils\DB_INIT('senpai');
+	 * @param string $prefix
+	 * @author amine safsafi
+	 * @return void
+	 */
+    public function __construct($prefix){
+        global $wpdb;
+        $this->senpai_db = $wpdb;
+        $this->senpai_prefix = $wpdb->prefix . $prefix . '_';
+        $this->senpai_charset = $wpdb->get_charset_collate();
+    }
+
+    public function create_table($table_name,$colums_sql){
+        $table = $this->senpai_prefix . $table_name;
+        $charset = $this->senpai_charset
+        $sql = "CREATE TABLE $table ($colums_sql) $charset;";
+        maybe_create_table( $table, $sql );
+    }
+
+    public function drop_table($table_name){
+        $table = $this->senpai_prefix . $table_name;
+        $sql = "DROP TABLE IF EXISTS $table";
+        $this->senpai_db->query($sql);
+    }
+    
+}
